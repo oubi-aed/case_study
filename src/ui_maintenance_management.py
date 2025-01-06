295b9f32a8334d0fcd6803cc4af64b237961cf9e
oubi-aed <alexander.oberhoer23@gmail.com>
2025-01-06 02:44:47 +0100

    Reserviersystem hinzugefügt

--- src/ui_maintenance_management.py
+++ src/ui_maintenance_management.py
@@ -8,30 +8,34 @@ from devices import Device
 def ui_maintenance_management():
 
     # Eine Überschrift der ersten Ebene
-    st.write("# Gerätemanagement")
+    st.write("# Wartungsmanager")
 
-    # Eine Überschrift der zweiten Ebene
-    st.write("## Geräteauswahl")
 
     # Eine Auswahlbox mit Datenbankabfrage, das Ergebnis wird in current_device gespeichert
     devices_in_db = find_devices()
 
     if devices_in_db:
+        #Dropdown Menü
         current_device_name = st.selectbox(
             'Gerät auswählen',
             options=devices_in_db, key="sbDevice_maintenance")
-
+        
+        #überprüft ob gültiges Gerät ausgewählt ist
         if current_device_name in devices_in_db:
+            #ladet Objekt aus der Datenbank
             loaded_device = Device.find_by_attribute("device_name", current_device_name)
+            #Ausgabe ob Objekt gefunden wurde oder nicht
             if loaded_device:
                 st.write(f"Loaded Device: {loaded_device}")
             else:
                 st.error("Device not found in the database.")
 
+            #laden des Textfeldes für Verantwortlichen
             with st.form(key="device_form"):
                 st.write(loaded_device.device_name)
-
+                #in value wird basierend auf Datenbank der eingegebene Wert gespeichert
                 text_input_val = st.text_input("Geräte-Verantwortlicher", value=loaded_device.managed_by_user_id)
+                #Aktualisiert das Gerätedatenobjekt mit dem neuen wert den der Benutzer eingegeben hat
                 loaded_device.set_managed_by_user_id(text_input_val)
 
                 # Every form must have a submit button.
